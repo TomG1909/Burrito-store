f709b9a6c7aa31bf0637ae1dbc79a91c1d301dc0
Toma Gela <toma.gela@gmail.com>
2022-04-19 12:09:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.class';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Burrito-store';
+
+  burritoCheese = new Product('Burrito Cheese', 9.50, 1);
+  burritoClassic = new Product('Burrito Classic', 8.00, 1)
 }
